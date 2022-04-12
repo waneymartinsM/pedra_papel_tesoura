eaeebeb434f88761044d0af9e73418a2dd695f57
Waney Martins <waneymartins2003@hotmail.com>
2022-04-12 11:43:11 -0300

    Initial commit

---
+++ lib/Jogo.dart
@@ -0,0 +1,125 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class Jogo extends StatefulWidget {
+  const Jogo({Key? key}) : super(key: key);
+
+  @override
+  _JogoState createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+
+  var _imagemApp = const AssetImage("imagens/padrao.png");
+  var _mensagem = "Escolha uma opção abaixo";
+
+  void _opcaoSelecionada(String escolhaUsuario){
+
+    var opcoes = ["pedra", "papel", "tesoura"];
+    var numero = Random().nextInt(3);
+    var escolhaApp = opcoes[numero];
+
+    print("Escolha do App: " + escolhaApp);
+    print("Escolha do usuario: " + escolhaUsuario);
+
+
+    //Exibição da imagem escolhida pelo App
+    switch( escolhaApp ){
+      case "pedra" :
+        setState(() {
+          _imagemApp = const AssetImage("imagens/pedra.png");
+        });
+        break;
+      case "papel" :
+        setState(() {
+          _imagemApp = const AssetImage("imagens/papel.png");
+        });
+        break;
+      case "tesoura" :
+        setState(() {
+          _imagemApp = const AssetImage("imagens/tesoura.png");
+        });
+        break;
+    }
+
+    //Validação do ganhador
+    //Usuario Ganhador
+    if(
+    (escolhaUsuario == "pedra" && escolhaApp == "tesoura") ||
+        (escolhaUsuario == "tesoura" && escolhaApp == "papel") ||
+        (escolhaUsuario == "papel" && escolhaApp == "pedra")
+    ){
+      setState(() {
+        _mensagem = "Parabéns! Você ganhou :)";
+      });
+      //App Ganhador
+    }else if(
+    (escolhaApp == "pedra" && escolhaUsuario == "tesoura") ||
+        (escolhaApp == "tesoura" && escolhaUsuario == "papel") ||
+        (escolhaApp == "papel" && escolhaUsuario == "pedra")
+    ){
+      setState(() {
+        _mensagem = "Você perdeu :(";
+      });
+    }else{
+      setState(() {
+        _mensagem = "Empatamos ;)";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber.shade300,
+        title: const Text("JokenPo"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              "Escolha do App",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Image(image: _imagemApp,),
+          Padding(
+            padding: const EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              _mensagem,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("pedra") ,
+                child: Image.asset("imagens/pedra.png", height: 100,),
+              ),
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("papel") ,
+                child: Image.asset("imagens/papel.png", height: 100,),
+              ),
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("tesoura") ,
+                child: Image.asset("imagens/tesoura.png", height: 100,),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:pedra_papel_tesoura/Jogo.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Jogo(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
